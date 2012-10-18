134d59dc9638072f0ad40a8b955f8e7acc037a01
David de Boer <david@ddeboer.nl>
2012-10-18 11:50:50 +0200

    Update path to Composer autoloader

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,14 +1,3 @@
 <?php
 
-$loader = require __DIR__. '/../vendor/.composer/autoload.php';
-
-spl_autoload_register(function($class) {
-    if (0 === strpos($class, 'Ddeboer\\Salesforce\\ClientBundle\\')) {
-        $path = __DIR__.'/../'.implode('/', array_slice(explode('\\', $class), 3)).'.php';
-        if (!stream_resolve_include_path($path)) {
-            return false;
-        }
-        require_once $path;
-        return true;
-    }
-});
+require __DIR__. '/../vendor/autoload.php';
